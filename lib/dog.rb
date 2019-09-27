fe3f65e616777fb5a7574d7d240e6fc3238d76ab
Tam Vu <vutam_a@hotmail.com>
2019-09-27 01:58:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
     dog_search = DB[:conn].execute("SELECT FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog_search.empty?
       dog_data = dog_search[0]
-      dog = Dog.new(dog_data)
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end 
